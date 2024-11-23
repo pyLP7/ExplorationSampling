d340c23f6b8e6a3684f3912cc779ac6f7a7da3bd
pyLP7 <73650883+pyLP7@users.noreply.github.com>
2024-11-23 18:00:20 +0000

    Add files via upload

--- src/adaptiveSampling.py
+++ src/adaptiveSampling.py
@@ -1,20 +1,22 @@
-import _DOE_class
+from utils import DOE_class
 
 if __name__ == "__main__":
     
-    doe_type = 'Sobol'
+    # define the dimension (dd, i.e. number of variables) and size (nn, max number of samples) of your DoE
+    dd = 2
+    nn = 16
     
+    # repeatability
     random_seed = 42
     
-    dd = 2
-    nn = 20
-    
-    gen_doe_sobol = _DOE_class.DOE_Class('sobol', repeat=random_seed).DOE
-    gen_doe_MIPT = _DOE_class.DOE_Class('MIPT', repeat=random_seed, sizeOneStage=10).DOE
-    gen_doe_MqPLHS = _DOE_class.DOE_Class('MqPLHS', repeat=random_seed, sizeOneStage=10).DOE
-    gen_doe_FpPLHS = _DOE_class.DOE_Class('FpPLHS', repeat=random_seed, sizeOneStage=10).DOE
+    # generate the 
+    # gen_doe_sobol = DOE_class.DOE_Class('sobol', repeat=random_seed).DOE
+    gen_doe_MIPT = DOE_class.DOE_Class('MIPT', repeat=random_seed, sizeOneStage=10).DOE
+    # gen_doe_MqPLHS = DOE_class.DOE_Class('MqPLHS', repeat=random_seed, sizeOneStage=10).DOE
+    # gen_doe_FpPLHS = DOE_class.DOE_Class('FpPLHS', repeat=random_seed, sizeOneStage=10).DOE
 
-    
+    # 
+    X = gen_doe_MIPT(dd,nn)
     # print(gen_doe_sobol(dd,nn))
     print(gen_doe_MIPT(dd,nn))
     # print(gen_doe_MqPLHS(dd,nn))
