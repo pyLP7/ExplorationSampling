fffb99a587f05027c9716c37a321de324e5adb18
pyLP7 <73650883+pyLP7@users.noreply.github.com>
2024-11-23 19:02:07 +0100

    Update adaptiveSampling.py

--- src/adaptiveSampling.py
+++ src/adaptiveSampling.py
@@ -9,16 +9,12 @@ if __name__ == "__main__":
     # repeatability
     random_seed = 42
     
-    # generate the 
-    # gen_doe_sobol = DOE_class.DOE_Class('sobol', repeat=random_seed).DOE
+    # Set up the DoE function
     gen_doe_MIPT = DOE_class.DOE_Class('MIPT', repeat=random_seed, sizeOneStage=10).DOE
     # gen_doe_MqPLHS = DOE_class.DOE_Class('MqPLHS', repeat=random_seed, sizeOneStage=10).DOE
     # gen_doe_FpPLHS = DOE_class.DOE_Class('FpPLHS', repeat=random_seed, sizeOneStage=10).DOE
 
-    # 
+    # Generate the DoE and store it in X
     X = gen_doe_MIPT(dd,nn)
-    # print(gen_doe_sobol(dd,nn))
-    print(gen_doe_MIPT(dd,nn))
-    # print(gen_doe_MqPLHS(dd,nn))
-    # print(gen_doe_FpPLHS(dd,nn))
+
     
